a62d3b888349f14885a04b205c72905591c120dd
Alexandre Roque <alexandre.roque1@hotmail.com>
2024-04-06 15:31:02 -0300

    feat: links para client/server side rendering

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -21,7 +21,7 @@ export default function ClientPage() {
 	}, []);
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center justify-between p-12 lg:p-24">
 			<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 				{products.map((product) => (
 					<Product product={product} key={product.id} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 	const products = await db.query.products.findMany();
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center justify-between p-12 lg:p-24">
 			<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 				{products.map((product) => (
 					<Product product={product} key={product.id} />

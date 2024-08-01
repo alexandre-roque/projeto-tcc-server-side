83ab52db70e1ee3ab3e1616a9b4f939b8e330ebe
Alexandre Roque <alexandre.roque1@hotmail.com>
2024-08-01 10:17:30 -0300

    fix: ajustar api de produtos para usar select

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,15 +1,17 @@
 export const revalidate = 0;
 import { db } from '@/db';
+import { products } from '@/db/schema/products';
 
 export async function POST(request: Request) {
 	const requestData = await request.json();
 
-	const products = await db.query.products.findMany({
-		limit: requestData.limit,
-	});
+	const productsResult = await db
+		.select()
+		.from(products)
+		.limit(requestData.limit || 10);
 
 	return Response.json({
-		products,
+		products: productsResult,
 	});
 }
 
@@ -17,11 +19,12 @@ export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const limit = searchParams.get('limit');
 
-	const products = await db.query.products.findMany({
-		limit: parseInt(limit || '10'),
-	});
+	const productsResult = await db
+		.select()
+		.from(products)
+		.limit(parseInt(limit || '10'));
 
 	return Response.json({
-		products,
+		products: productsResult,
 	});
 }

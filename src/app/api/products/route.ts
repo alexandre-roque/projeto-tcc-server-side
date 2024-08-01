eff09b2bcd4af8814e06e7730aa52c1b47283cb8
Alexandre Roque <alexandre.roque1@hotmail.com>
2024-08-01 09:58:00 -0300

    fix: ajustar api de produtos

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,13 +1,40 @@
-import { NextResponse } from "next/server";
-import { db } from "@/db";
+export const revalidate = 0;
+
+import { db } from '@/db';
 
 export async function POST(request: Request) {
 	const requestData = await request.json();
 
 	const products = await db.query.products.findMany({
+		with: {
+			imageUrl: true,
+			name: true,
+			description: true,
+			price: true,
+		},
 		limit: requestData.limit,
 	});
-	return NextResponse.json({
+
+	return Response.json({
+		products,
+	});
+}
+
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const limit = searchParams.get('limit');
+
+	const products = await db.query.products.findMany({
+		with: {
+			imageUrl: true,
+			name: true,
+			description: true,
+			price: true,
+		},
+		limit: parseInt(limit || '10'),
+	});
+
+	return Response.json({
 		products,
 	});
 }
